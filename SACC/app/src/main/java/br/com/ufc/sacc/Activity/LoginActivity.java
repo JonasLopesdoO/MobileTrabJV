cb0b6ea9886d3819d21460cad09b57984b5ca86a
Jonas Lopes <jonaslopesdoo@hotmail.com>
2018-10-26 17:23:36 -0300

    tentativa facebook

--- SACC/app/src/main/java/br/com/ufc/sacc/Activity/LoginActivity.java
+++ SACC/app/src/main/java/br/com/ufc/sacc/Activity/LoginActivity.java
@@ -3,6 +3,7 @@ package br.com.ufc.sacc.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,9 +13,17 @@ import android.widget.Toast;
 import br.com.ufc.sacc.DAO.ConfiguracaoFirebase;
 import br.com.ufc.sacc.Model.Usuario;
 import br.com.ufc.sacc.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -23,8 +32,11 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edtSenha;
     private Button btnLogar;
     private TextView tvAbreCadastro;
+    private LoginButton mLoginButtonFb;
+    private CallbackManager mCallBackManager;
 
     private FirebaseAuth autenticacao;
+    private FirebaseAuth mFirebaseAuth;
     private Usuario usuario;
 
     @Override
@@ -38,7 +50,6 @@ public class LoginActivity extends AppCompatActivity {
         tvAbreCadastro = findViewById(R.id.tvAbreCadastro);
 
         Toast.makeText(LoginActivity.this, "Digite os dados de login", Toast.LENGTH_SHORT).show();
-
 
         btnLogar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +70,63 @@ public class LoginActivity extends AppCompatActivity {
                 abrirTelaCadastroUsuario();
             }
         });
+
+        inicializarComponenteFacebook();
+        inicializarFirebaseCallBack();
+        clickButtonFacebook();
+    }
+
+    private void clickButtonFacebook() {
+        mLoginButtonFb.registerCallback(mCallBackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                firebaseLoginFacebook(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                alert("Operação cancelada");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                alert("Erro ao logar com o facebook, tente novamente");
+            }
+        });
+    }
+
+    private void firebaseLoginFacebook(AccessToken accessToken) {
+        AuthCredential credencial = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mFirebaseAuth.signInWithCredential(credencial).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(LoginActivity.this, PrincipalActivity.class);
+                    startActivity(intent);
+                }else{
+                    alert("Erro de autenticação com o Servidor.");
+                }
+
+            }
+        });
+
+    }
+
+    private void inicializarFirebaseCallBack() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mCallBackManager = CallbackManager.Factory.create();
+        //criar uma transposição para o Singleton depois
+    }
+
+    private void inicializarComponenteFacebook() {
+        mLoginButtonFb = findViewById(R.id.btnLogarFacebook);
+        mLoginButtonFb.setReadPermissions("email", "public_profile");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallBackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     private void validarLogin(){
@@ -92,15 +160,16 @@ public class LoginActivity extends AppCompatActivity {
 
     private void abrirTelaPrincipal(){
         Intent intentAbrirTelaPrincipal = new Intent(LoginActivity.this, PrincipalActivity.class);
-        //intentAbrirTelaPrincipal.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intentAbrirTelaPrincipal);
         onBackPressed();
-        //finish();
     }
-
 
     public void abrirTelaCadastroUsuario(){
         Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
         startActivity(intent);
+    }
+
+    private void alert(String operação_cancelada) {
+        Toast.makeText(LoginActivity.this, operação_cancelada, Toast.LENGTH_LONG).show();
     }
 }
