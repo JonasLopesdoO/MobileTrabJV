8060ad2ec484307d06e54d966e17d2965a04203a
Jonas Lopes <jonaslopesdoo@hotmail.com>
2018-11-03 10:25:46 -0300

    Ícones novos para funcionar

--- SACC/app/src/main/java/br/com/ufc/sacc/Activity/PrincipalActivity.java
+++ SACC/app/src/main/java/br/com/ufc/sacc/Activity/PrincipalActivity.java
@@ -22,7 +22,7 @@ public class PrincipalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer) ;
+        mDrawerLayout = findViewById(R.id.drawer) ;
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
