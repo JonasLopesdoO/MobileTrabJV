eac031035379d5af4ce0454bece3576c799182ac
Jonas Lopes <jonaslopesdoo@hotmail.com>
2018-11-03 23:21:25 -0300

    Tudo funfando sem a navigation

--- SACC/app/src/main/java/br/com/ufc/sacc/Activity/PrincipalActivity.java
+++ SACC/app/src/main/java/br/com/ufc/sacc/Activity/PrincipalActivity.java
@@ -2,11 +2,7 @@ package br.com.ufc.sacc.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import br.com.ufc.sacc.R;
@@ -14,18 +10,18 @@ import br.com.ufc.sacc.R;
 public class PrincipalActivity extends AppCompatActivity {
 
     Button btnLogout;
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
+    //private DrawerLayout mDrawerLayout;
+    //private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        mDrawerLayout = findViewById(R.id.drawer) ;
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
+        //mDrawerLayout = findViewById(R.id.drawer) ;
+        //mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        //mDrawerLayout.addDrawerListener(mToggle);
+        //mToggle.syncState();
 
 
         btnLogout = findViewById(R.id.btnLogout);
@@ -44,35 +40,35 @@ public class PrincipalActivity extends AppCompatActivity {
         });
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
-        if (mToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+   // public boolean onOptionsItemSelected(MenuItem item){
+     //   if (mToggle.onOptionsItemSelected(item)){
+       //     return true;
+        //}
+    //return super.onOptionsItemSelected(item);
+    //}
 
-    public boolean onNavigationItemSelected(MenuItem item){
-        int id = item.getItemId();
+    //public boolean onNavigationItemSelected(MenuItem item){
+    //    int id = item.getItemId();
 
-        if (id == R.id.perfil){
-            Intent intentPerfil = new Intent(getApplicationContext(), PerfilActivity.class);
-            startActivity(intentPerfil);
-        } else if (id == R.id.mensagem){
-            Intent intentMensagem = new Intent(getApplicationContext(), MensagemActivity.class);
-            startActivity(intentMensagem);
-        } else if (id == R.id.faq){
-            Intent intentFaq= new Intent(getApplicationContext(), FaqActivity.class);
-            startActivity(intentFaq);
-        } else if (id == R.id.como_chegar){
-            Intent intentComoChegar= new Intent(getApplicationContext(), ComoChegarActivity.class);
-            startActivity(intentComoChegar);
-        }
+    //    if (id == R.id.perfil){
+    //      Intent intentPerfil = new Intent(getApplicationContext(), PerfilActivity.class);
+    //       startActivity(intentPerfil);
+    //    } else if (id == R.id.mensagem){
+    //      Intent intentMensagem = new Intent(getApplicationContext(), MensagemActivity.class);
+    //       startActivity(intentMensagem);
+    //  } else if (id == R.id.faq){
+    //      Intent intentFaq= new Intent(getApplicationContext(), FaqActivity.class);
+    //     startActivity(intentFaq);
+    // } else if (id == R.id.como_chegar){
+    //     Intent intentComoChegar= new Intent(getApplicationContext(), ComoChegarActivity.class);
+    //     startActivity(intentComoChegar);
+    // }
 
-        DrawerLayout drawer = findViewById(R.id.drawer);
-        drawer.closeDrawer(GravityCompat.START);
+    //   DrawerLayout drawer = findViewById(R.id.drawer);
+    //  drawer.closeDrawer(GravityCompat.START);
 
-        return true;
-    }
+    // return true;
+    //}
 
     @Override
     public void onBackPressed() {
